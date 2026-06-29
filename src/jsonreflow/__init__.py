fd2ac960bcc7b41dd033bebc247eb3c72d0fbe3d
Stefaan Lippens <soxofaan@gmail.com>
2026-06-29 15:58:41 +0200

    add reflow_file to jsonreflow/__init__.py

--- src/jsonreflow/__init__.py
+++ src/jsonreflow/__init__.py
@@ -1,5 +1,5 @@
-from jsonreflow.reflow import dump, dumps, reflow, reflow_iter
+from jsonreflow.reflow import dump, dumps, reflow, reflow_file, reflow_iter
 
 __version__ = "0.7.0"
 
-__all__ = ["reflow_iter", "reflow", "dumps", "dump", "__version__"]
+__all__ = ["reflow_iter", "reflow", "dumps", "dump", "reflow_file", "__version__"]
